f7bfadc42a6c77500c3792ec68884bc385041764
Nathan <nathanieljones@172.21.16.79>
2015-04-02 18:05:51 -0700

    Ok, after much effort, I've gotten resistivity and UV luminance index to compare using an iPoint core/log shift and it looks like there is a statistical relationship. :)

--- CurrentRunScripts/UV_to_ImageComparison/CompareUVtoImagePadRes.m
+++ CurrentRunScripts/UV_to_ImageComparison/CompareUVtoImagePadRes.m
@@ -3,6 +3,19 @@ clear all
 
 %%Load resisitivity pad info
 ResPadFilename = uigetfile('*.txt', 'Select Pad Resistivity File');
+%load iPoint shifts info
+try
+shiftsFile = uigetfile('*.csv', 'Select ipoint Core Shifts Info .csv');
+shifts = simpleCSVImport(shiftsFile);
+[a,~] = size(shifts);
+shifts = shifts(5:a,:);
+shifts = str2double(shifts);
+clear a;
+catch err
+    warndlg('Warning! shifts file not loaded, unrecognized format.');
+    
+end
+clear shiftsFile;
 try
 [ resPadFileOutput ] = simpleTabImport( ResPadFilename);
 clear ResPadFilename;
@@ -45,12 +58,139 @@ end
 images = images(idx);
 %%Upscale UV data to the scale of the resisitivity pad data
 padVertResolution = resData(2,1) - resData(1,1);
+resStart = resData(1,1);
+% %load iPoint shifts info
+% shiftsFile = uigetfile('*.csv', 'Select ipoint Core Shifts Info .csv');
 
+for x = 1:length(images)
+%%load image
+imageData = imread(images{1,x});
+[~,~,~,~,~,TopDepth,BottomDepth,~,~,~,~,~,~,~] = importCorePhotoCSV(metadata{1,x});
+%Find initial shift
+count = 0;
+imageToLogShifts = zeros(1,2);
+found = false;
+ftPerPix = (BottomDepth - TopDepth)/size(imageData,1);
+for y = 1:length(shifts)
+    if shifts(y,2) > 40
+        disp('Skipping a shift with >40ft size');
+        continue;
+    end
+    if (shifts(y,1) >= TopDepth) && (y ~= 1)
+         if shifts(y-1,2) > 40
+        disp('Skipping a shift with >40ft size');
+            stepBack = 2;
+         else
+             stepBack = 1;
+         end
+    
+        count = count + 1;
+        found = true;
+        imageToLogShifts(count,1) = shifts(y-stepBack ,1);
+        imageToLogShifts(count,2) = shifts(y-stepBack ,2);
+        break;
+    end
+end
+if ~found
+    shiftDiffs = zeros(1,1);
+    for y = 1:length(shifts)
+        if shifts(y,2) > 40
+            disp('Skipping a shift with >40ft size');
+            continue;
+        end
+        shiftDiffs(y,1) = TopDepth - shifts(y,1);
+        if shiftDiff(y,1) < 0
+            shiftDiffs(y,1) = 100000;
+        end
+    end
+    [~,I] = min(shiftDiffs);
+    count = count + 1;
+   imageToLogShifts(count,1) = shifts(I,1);
+   imageToLogShifts(count,2) = shifts(I,2);
+end
+%%find number of shifts contained in the image interval
 
+for y = 1:length(shifts)
+    if shifts(y,2) > 40
+        disp('Skipping a shift with >40ft size');
+        continue;
+    end
+    if (shifts(y,1) >= TopDepth) && (shifts(y,1) <= BottomDepth)
+        count = count + 1;
+        imageToLogShifts(count,1) = shifts(y,1);
+        imageToLogShifts(count,2) = shifts(y,2);
+    end
+end
+lastPixelShift = 0;
+for y = 1:count  
+%%Filter the core image data to the desired interval
+%start of shift
+shiftStartPixels = round((imageToLogShifts(y,1) - TopDepth) / ftPerPix);
+if shiftStartPixels <= 0
+    startCoreDepth = TopDepth;
+    shiftStartPixels = 1;
+else
+    startCoreDepth = imageToLogShifts(y,1);
+end
+%end of shift
+if y == count
+  shiftEndPixels = size(imageData,1);
+  endCoreDepth = BottomDepth;
+else
+  shiftEndPixels = round((imageToLogShifts(y+1,1) - TopDepth) / ftPerPix);
+  endCoreDepth = imageToLogShifts(y+1,1);
+end
+selectedImageData = imageData(shiftStartPixels:shiftEndPixels,:,:);
+%get equivalent image log pad data
+logStartDepth = startCoreDepth + imageToLogShifts(y,2);
+
+if y == count
+    logEndDepth = endCoreDepth + imageToLogShifts(y,2);
+else
+    logEndDepth = endCoreDepth + imageToLogShifts(y+1,2);
+end
+startFound = false;
+endFound = false;
+for z = 1:length(resData)
+    if startFound && endFound
+        break;
+    end
+    if (resData(z,1) > logStartDepth) && ~startFound
+        resStartIndex = z;
+        startFound = true;
+    end
+    if (resData(z,1) > logEndDepth) && ~endFound
+        resEndIndex = z;
+        endFound = true;
+    end
+    
+end
+if ~endFound
+    resEndIndex = z;
+end
+selectedImageLogData = resData(resStartIndex:resEndIndex, :);
 %%Decimate the image being used for comparison (selected image interval)
+scale = ftPerPix / padVertResolution;
+resizedSelectedImageData = imresize(selectedImageData,scale);
+%convert to luminosity
+luminosity= resizedSelectedImageData(:,:,1) .* 0.2126 + resizedSelectedImageData(:,:,2) .* 0.7152 + resizedSelectedImageData(:,:,3) .* 0.0722;
 
-%%Filter the image data to the desired interval
+%size the luminosity to the image log data
+luminositySize = size(luminosity,1);
+resistivitySize = size(selectedImageLogData, 1);
+if luminositySize > resistivitySize
+    luminosity = luminosity(1:resistivitySize, :);
+elseif luminositySize < resistivitySize
+    selectedImageLogData = selectedImageLogData(1:luminositySize, :);
+end
 
+%QC: Plot image resistivity vs. luminosity index
+scatter(luminosity(:,round(size(luminosity,2)/2)), selectedImageLogData(:,2));
+%accumulate the results into results arrays for unified plotting
+
+end
+
+end
 
 %%%TODO: ALSO MAKE A VERSION USING iPOINT shifts as an input%%%
 
